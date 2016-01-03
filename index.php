9e4bf6b8294e7a94897eaf507fed669b5c18a3e3
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-01-03 14:34:51 +0000

    Update index.php
    Set first form

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<?php
+
 print <<<_html_
 
-<form method="post" action="">
+<h1>Primes up to chosen number</h1>
+<form method="post" action="primesto.php">
 <input type="text" name="input">
 <input type="submit">
 </form>
 
 
+
+
 _html_;
+?>
