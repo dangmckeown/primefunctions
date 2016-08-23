0ab6c367b92284914a32950d59590ad4f1261b56
dangmckeown <daniel.mckeown@gmail.com>
2016-08-23 10:42:51 +0100

    RWD added

--- index.php
+++ index.php
@@ -7,16 +7,17 @@
 
 	<style>
 
-@media-screen and (min-width:712px){
-body{margin-left: 20%;}
+@media screen and (min-width:712px){
+#main{margin-left: 20%;}
 }
 
 
-@media-screen and (min-width:512px){
+@media screen and (min-width:512px){
 #optimus_prime_pic{width:512px;}
 }
 
-@media-screen and (max-width:511px){
+@media screen and (max-width:511px){
+#main{width:100%;}
 #optimus_prime_pic{width:100%;}
 }
 </style>
@@ -26,7 +27,7 @@ body{margin-left: 20%;}
 
 <p>Preambulatory paragraph here</p>
 </center>
-
+<div id="main">
 <h2>Primes up to chosen number</h2>
 <form method="post" action="primesto.php">
 <input type="text" name="input">
@@ -48,6 +49,6 @@ body{margin-left: 20%;}
 </form>
 
 <a title="By Hiroyuki Obara. (Patent number 4516948) [Public domain], via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File%3AOptimus_Prime_patent.png"><img id="optimus_prime_pic" alt="Optimus Prime patent" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Optimus_Prime_patent.png/512px-Optimus_Prime_patent.png"/></a>
-
+</div>
 </body>
 </html>
