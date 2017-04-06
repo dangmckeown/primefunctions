c8d869211e5e35e3b450f38dd112fc70d80e11a1
Dan McKeown <dangmckeown@users.noreply.github.com>
2017-04-06 13:09:52 +0100

    Debug

--- primesgame.php
+++ primesgame.php
@@ -4,10 +4,13 @@ include_once('primefunctions.php');
 
 // Generate number
 function generate_number($turn){
+  echo "function invoked!";
 $endings = ([1,3,5,7,9]);
 $nums = ([0,1,2,3,4,5,6,7,8,9]);
 
 $figs = strlen(decbin($turn));
+  
+  echo $figs;
 
 $output = "";
 
